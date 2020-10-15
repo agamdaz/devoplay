e28903302e1fce10afcb364afc18ca11da83b3fc
Aurelio Gamero <aureliogamero@gmail.com>
2020-10-15 16:57:27 +0200

    Updates

---
+++ precache-manifest.5f8810c3960b835faf2d41265acf9dfc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83748dc33ef0e9b6a7b75f05c419ece7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.10a85211028ecd5f67e4.bundle.js"
+  },
+  {
+    "url": "runtime~main.10a85211028ecd5f67e4.bundle.js"
+  },
+  {
+    "url": "vendors~main.10a85211028ecd5f67e4.bundle.js"
+  },
+  {
+    "url": "vendors~main.10a85211028ecd5f67e4.bundle.js.LICENSE.txt"
+  }
+]);
